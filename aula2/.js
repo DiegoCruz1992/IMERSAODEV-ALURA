268b5e304a748da4eee7166fcb097389e7641df6
Diego Mauricio da Cruz <78426489+DiegoCruz1992@users.noreply.github.com>
2021-04-07 14:53:44 -0300

    aula2

---
+++ aula2/.js
@@ -0,0 +1,28 @@
+var primeiroValor = parseInt(prompt("Digite o primeiro valor:"))
+var segundoValor = parseInt(prompt("Digite o segundo valor:"))
+
+var operacao = parseInt(prompt("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração: "))
+
+if (operacao === 1) {
+  var resultado = primeiroValor / segundoValor
+  document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao === 2) {
+  var resultado = primeiroValor * segundoValor
+  document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao === 3) {
+  var resultado = primeiroValor + segundoValor
+  document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + resultado + "</h2>")
+} else if (operacao == 4) {
+  var resultado = primeiroValor - segundoValor
+  document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + resultado + "</h2>")
+} else {
+  document.write("<h2>Opção inválida</h2>")
+}
+
+//escrevendo na tela - document.write()
+//concatenação (juntar palavra com variáveis) - ("palavra" + variavel)
+// == - comparação é diferente do = que usamos para fazer atribuição
+
+//if = se
+//else = senão
+//else if = senão se
